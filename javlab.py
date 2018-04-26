d5e7855d7e6e87b7022198fbc5daea7c3780477c
lienzh <zjtzjjlan@gmail.com>
2018-04-27 00:24:57 +0800

    combine javlab&webdata

--- javlab.py
+++ javlab.py
@@ -1,8 +1,27 @@
 # -*- coding:utf-8 -*-
 import re
+import urllib
+import urllib2
+ 
+url = 'http://www.javlibrary.com/tw/vl_bestrated.php'
+#url = 'http://www.cool18.com/bbs4/index.php'
+#url ='http://g.91p11.space/viewthread.php?tid=272348'
+user_agent = 'Mozilla/4.0 (compatible; MSIE 5.5; Windows NT)'
+headers = { 'User-Agent' : user_agent }
 
-with open('javlab.html','rb') as f:
-    data = f.read()
+def getdata(url=url, headers=headers):
+    try:
+        request = urllib2.Request(url,headers = headers)
+        response = urllib2.urlopen(request)
+#    print response.read()
+        with open('file.txt','wb') as f:
+            f.write(response.read())
+    except urllib2.URLError, e:
+        if hasattr(e,"code"):
+            print e.code
+        if hasattr(e,"reason"):
+    	    print e.reason
+
 
 #获取番号、片名、演员、图片
 #re.S，.也可以表示换行符
@@ -20,4 +39,7 @@ def getav(data):
         print "----------------------"
 
 if __name__ == "__main__":
+    getdata()
+    with open('file.txt','rb') as f:
+        data = f.read()
     getav(data)
